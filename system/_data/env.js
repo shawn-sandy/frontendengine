3937b817d4a1b34616972e1e6d3251523aceb235
Shawn Sandy <creative@shawnsandy.design>
2021-04-13 05:55:53 -0400

    Fix: typo in developer_mode statement in env.js

--- system/_data/env.js
+++ system/_data/env.js
@@ -5,7 +5,7 @@ module.exports = {
   node_env: process.env.NODE_ENV || null,
   site_map_url: process.env.SITE_MAP_URL || 'https//site.com',
   contact_email: process.env.CONTACT_EMAIL || 'site@amce.inc',
-  contact_success: process.CONTACT_SUCCESS || '/thank-you',
-  developer_mode: process.DEVELOPER_MODE || true,
+  contact_success: process.env.CONTACT_SUCCESS || '/thank-you',
+  developer_mode: process.env.DEVELOPER_MODE || true,
   form_provider: process.env.FORM_PROVIDER || null
 }
